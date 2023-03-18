a1b87da513972b700763c11bbb6f1e8a2ac5df6c
Taofeek-ATom <alitaofeek38@gmail.com>
2023-03-18 12:14:41 -0700

    using the put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* put - using function put
+* return:  value (0) sucees;
+* main - entry space
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
