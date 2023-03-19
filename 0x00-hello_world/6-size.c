bd71824bfb9687d26e1e1cc696d263a1aba7ea62
Taofeek-ATom <alitaofeek38@gmail.com>
2023-03-19 05:30:08 -0700

    size of all data-types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - this is the main
+* print - prints "size of data-type"
+* Return: 0 always as positive
+*/
+int main(void)
+{
+	char b;
+	int i;
+	float d;
+	long int f;
+	long long int g;
+
+	printf("size of a char is: %lu.\n", (unsigned long)sizeof(b));
+	printf("size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("size of a float is: %lu.\n", (unsigned long)sizeof(d));
+	printf("size of long int is: %lu.\n", (unsigned long)sizeof(f));
+	printf("size of long long int is: %lu.\n", (unsigned long)sizeof(g));
+	return (0);
+}
