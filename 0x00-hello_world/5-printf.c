b17a5ef5be1678551c108099308589ba89cdc2dc
Taofeek-ATom <alitaofeek38@gmail.com>
2023-03-18 23:22:01 -0700

    priniting the first statement

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - this is the main
+* print - prints "with proper grammar, but the outcome is a piece of art,"
+* Return: 0 always as positive
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
